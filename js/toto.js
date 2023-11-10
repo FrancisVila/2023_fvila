d584f191fbed0c383f62519c96fb11e148cc88bc
Francis Vila <fvila@axway.com>
2023-11-10 17:22:35 +0100

    svg avec bordure blanche, changé "mon site"

--- js/toto.js
+++ js/toto.js
@@ -18,12 +18,34 @@ var $grid = $('.grid2').isotope({
 
 $grid.imagesLoaded().progress( function() {
   
-
-  console.log('TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT')
   $grid.isotope('layout');
   
 });  
 
+
+let touchstartX = 0
+let touchendX = 0
+const next = document.getElementById("nextUrl").value; 
+const prev = document.getElementById("prevUrl").value; 
+console.log("prev 111111111111111", prev)
+function checkDirection() {
+  touchDistance = Math.abs(touchendX - touchstartX)
+  if (touchendX < touchstartX && touchDistance > 100) {
+    window.location = prev;
+  }
+  if (touchendX > touchstartX && touchDistance > 100) {
+    window.location = next;
+  }
+}
+
+document.addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX
+})
+
+document.addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX
+  checkDirection()
+})
 
 // const toto =()=> {alert('click!')  }
 
